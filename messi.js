818aa6209a52d9534e9483e7977c098004e57db3
Jason Ascencio <101837083+JasonASoftware@users.noreply.github.com>
2022-12-06 17:01:06 -0500

    created js file

---
+++ messi.js
@@ -0,0 +1,6 @@
+//created 4 variables with each on how many goals he scored on different competitions 
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+//add the sum of all the goals he scored
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
